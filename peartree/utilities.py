a5d5da6129dd6efceb4320875587eaa7ab58c468
Kuan Butts <kuanbutts@gmail.com>
2017-11-18 08:54:19 -0800

    move peartree up one folder

--- peartree/utilities.py
+++ peartree/utilities.py
@@ -1,5 +1,7 @@
 import logging as lg
 
+import partridge as ptg
+
 
 def config(log_console=settings.log_console):
     # Taken from OSMnx's utils.py file, see log comments
@@ -13,7 +15,7 @@ def config(log_console=settings.log_console):
         log('Configured osmnx')
 
 
-def log(message, level=None, name=None, filename=None):
+def log(message: str, level=None, name=None, filename=None):
     # Same function, taken from OSMnx's log utility
     # Link: https://github.com/gboeing/osmnx/blob/
     #       0f284ae78ccbf732f5550f96d0deebe287dab115/osmnx/utils.py#L95
@@ -53,3 +55,50 @@ def log(message, level=None, name=None, filename=None):
         message = unicodedata.normalize('NFKD', make_str(message)).encode('ascii', errors='replace').decode()
         print(message)
         sys.stdout = standard_out
+
+
+def get_representative_feed(day_type: str, file_loc: str, name: str=None):
+    """
+    Get a subset of the feed by a specific type of day (e.g. busiest).
+
+    Parameters
+    ----------
+    day_type : string
+        a supported categorical that will indicate what kind day you want from the schedule
+        as determined by schedule frequency
+    file_loc : string
+        location path to the zip file of the gtfs
+    name : string
+        the name of the feed being produced (if none, will auto-generate)
+
+    Returns
+    -------
+    feed : a partridge object representing a subset of a transit feed for a given schedule id
+    """
+
+
+
+    # Generate a random name for name if it is None
+    if not name:
+        name = _generate_random_name()
+    
+    service_ids_by_date = ptg.read_service_ids_by_date(gtfs_loc)
+    trip_counts_by_date = ptg.read_trip_counts_by_date(gtfs_loc)
+
+    # At this point, different methods can be implemented to help select how
+    # to pick which date/schedule id to use
+
+    if day_type == 'busiest':
+        (selected_date,
+         trip_count) = max(trip_counts_by_date.items(), key=lambda p: p[1])
+    else:
+        raise NotImplementedError('Unsupported day type option supplied.')
+
+    log(f'Selected_date: {selected_date}')
+    log(f'Number of trips on that date: {trip_count}')
+
+    all_service_ids = '\n\t'.join(service_ids_by_date[selected_date])
+    log(f'\nAll related service IDs: \n\t{all_service_ids}')
+
+    feed_query = {'trips.txt': {'service_id': service_ids_by_date[selected_date]}}
+    return ptg.feed(gtfs_loc, view=feed_query)
