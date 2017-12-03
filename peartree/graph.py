d25d0902c04588decb1e1c9c893267a7c4df24d3
Kuan Butts <kuanbutts@gmail.com>
2017-12-03 11:12:26 -0800

    drop extraneous var

--- peartree/graph.py
+++ peartree/graph.py
@@ -9,6 +9,7 @@ from .settings import WGS84
 from .summarizer import (generate_edge_and_wait_values,
                          generate_summary_edge_costs,
                          generate_summary_wait_times)
+from .toolkit import generate_graph_node_dataframe, get_nearest_node
 
 
 def generate_empty_md_graph(name: str,
@@ -36,11 +37,61 @@ def generate_summary_graph_elements(feed: ptg.gtfs.feed,
     return (summary_edge_costs, wait_times_by_stop)
 
 
+def generate_cross_feed_edges(G,
+                              feed,
+                              wait_times_by_stop,
+                              connection_threshold):
+    # First, we need a DataFrame representation of the nodes in the graph
+    node_df = generate_graph_node_dataframe(G)
+
+    stop_ids = []
+    to_nodes = []
+    edge_costs = []
+
+    # TODO: Repeating this in populate_graph as well, there may
+    #       be a way to condense these two steps well
+    for i, row in wait_times_by_stop.iterrows():
+        sid = str(row.stop_id)
+
+        # TODO: Join tables before hand to make
+        #       this part go faster
+        id_mask = (feed.stops.stop_id == sid)
+        stop_data_head = feed.stops[id_mask].head(1)
+
+        # Handle the possibility that there are no values for that stop
+        # id in the feed subset of wait times
+        if not len(stop_data_head):
+            continue
+
+        # Once check has cleared, pull out the first row as a pd.Series
+        stop_data = stop_data_head.T.squeeze()
+
+        point = (stop_data.stop_lat, stop_data.stop_lon)
+        (nn, nn_dist) = get_nearest_node(node_df, point)
+
+        # Only generate a connector edge if it satisfies the
+        # meter distance threshold
+        if nn_dist < connection_threshold:
+            stop_ids.append(sid)
+            to_nodes.append(nn)
+            edge_costs.append(nn_dist)
+
+    return pd.DataFrame({'stop_id': stop_ids,
+                         'to_node': to_nodes,
+                         'distance': edge_costs})
+
+
 def populate_graph(G: nx.MultiDiGraph,
                    name: str,
                    feed: ptg.gtfs.feed,
                    wait_times_by_stop: pd.DataFrame,
-                   summary_edge_costs: pd.DataFrame):
+                   summary_edge_costs: pd.DataFrame,
+                   cross_feed_edges: pd.DataFrame):
+    # As we convert stop ids to actual nodes, let's keep track of those names
+    # here so that we can reference them when we add connector edges across
+    # the various feeds loaded into the graph
+    sid_lookup = {}
+
     for i, row in wait_times_by_stop.iterrows():
         sid = str(row.stop_id)
         full_sid = nameify_stop_id(name, sid)
@@ -48,7 +99,18 @@ def populate_graph(G: nx.MultiDiGraph,
         # TODO: Join tables before hand to make
         #       this part go faster
         id_mask = (feed.stops.stop_id == sid)
-        stop_data = feed.stops[id_mask].head(1).T.squeeze()
+        stop_data_head = feed.stops[id_mask].head(1)
+
+        # Handle the possibility that there are no values for that stop
+        # id in the feed subset of wait times
+        if not len(stop_data_head):
+            continue
+
+        # Once check has cleared, pull out the first row as a pd.Series
+        stop_data = stop_data_head.T.squeeze()
+
+        # Add to the lookup crosswalk dictionary
+        sid_lookup[sid] = full_sid
 
         G.add_node(full_sid,
                    boarding_cost=row.avg_cost,
@@ -61,5 +123,24 @@ def populate_graph(G: nx.MultiDiGraph,
         G.add_edge(sid_fr,
                    sid_to,
                    length=row.edge_cost)
+
+    # Now add the cross feed edge connectors to the graph to
+    # capture transfer points
+    for i, row in cross_feed_edges.iterrows():
+        # Extract the row column values as discrete variables
+        sid = row.stop_id
+        to = row.to_node
+        d = row.distance
+
+        # Use the lookup table to get converted stop id name
+        full_sid = sid_lookup[sid]
+
+        # Convert to km/hour
+        kmph = (d / 1000) / 4.5
+
+        # Convert to seconds
+        in_seconds = kmph * 60 * 60
+
+        G.add_edge(full_sid, to, length=in_seconds)
 
     return G
