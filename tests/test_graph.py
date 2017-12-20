426c1775cbe7b4081bbd68428227edf21e256d13
Kuan Butts <kuanbutts@gmail.com>
2017-12-20 10:49:56 -0800

    graph empty create test

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -1,1 +1,7 @@
-import pytest
+from peartree.graphs import generate_empty_md_graph
+
+
+def test_generate_empty_graph():
+    G = generate_empty_md_graph('foo')
+    assert len(G.edges()) == 0
+    assert len(G.nodes()) == 0
