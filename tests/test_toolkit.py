33b2b65d5daba250d0d403dc1d58f4f54ce5291b
Kuan Butts <kuanbutts@gmail.com>
2018-03-20 05:58:51 +0800

    Coalesce graph operation (#45)
    * Coalesce graph operation
    
    * mod to handle py35

--- tests/test_toolkit.py
+++ tests/test_toolkit.py
@@ -1,6 +1,15 @@
 import networkx as nx
 import pytest
-from peartree.toolkit import reproject
+from peartree.toolkit import coalesce, reproject
+
+
+def _dict_equal(got, want):
+    all_valid = True
+    for key in want:
+        if not got[key] == want[key]:
+            all_valid = False
+
+    return all_valid
 
 
 def test_feed_to_graph_plot():
@@ -22,9 +31,7 @@ def test_feed_to_graph_plot():
         ys.append(node['y'])
 
     # TODO: Can we improve how this is assessed? Seems like G.nodes()
-    #       does not retun the nodes in the same order each time; there
-    #       is a component of the node and edge creation that is not
-    #       deterministic and this is a hack to circumvent that issue.
+    #       does not return nodes in same order in Py 3.6 as in 3.5
     expected_xs = [-1932968.345, -1932884.818]
     for x in xs:
         a = (x == pytest.approx(expected_xs[0], abs=0.01))
@@ -36,3 +43,35 @@ def test_feed_to_graph_plot():
         a = (y == pytest.approx(expected_ys[0], abs=0.01))
         b = (y == pytest.approx(expected_ys[1], abs=0.01))
         assert (a or b)
+
+
+def test_coalesce_operation():
+    # Create a simple graph
+    G = nx.Graph(crs={'init': 'epsg:4326', 'no_defs': True}, name='foo')
+
+    # And add two nodes to it
+    G.add_node('a', x=-122.2729918, y=37.7688136, boarding_cost=10)
+    G.add_node('b', x=-122.2711039, y=37.7660709, boarding_cost=15)
+    G.add_node('c', x=-122.2711038, y=37.7660708, boarding_cost=12)
+    G.add_edge('a', 'b', length=10, mode='transit')
+    G.add_edge('b', 'c', length=1, mode='walk')
+
+    G2 = reproject(G)
+
+    G2c = coalesce(G2, 200)
+    G2c.nodes(data=True), G2c.edges(data=True)
+
+    # Same akward situation as before, where edges are returned in
+    # different order between Py 3.5 and 3.6
+    for i, node in G2c.nodes(data=True):
+        a = _dict_equal(node, {
+            'x': -1933000, 'y': -543000, 'boarding_cost': 10.0})
+        b = _dict_equal(node, {
+            'x': -1932800, 'y': -543400, 'boarding_cost': 13.5})
+        assert (a or b)
+
+    all_edges = list(G2c.edges(data=True))
+    assert len(all_edges) == 1
+
+    # Make sure that the one edge came out as expected
+    assert _dict_equal(all_edges[0][2], {'length': 10, 'mode': 'transit'})
