fda9ced002baa6b04227ebf44d02ea60462c3909
Kuan Butts <kuanbutts@gmail.com>
2018-06-04 16:35:08 -0700

    Adjust the way plot method uses MPL (#75)
    * Add a description
    
    * default agg use for mpl
    
    * use agg adjust
    
    * use agg trigger adjust
    
    * udpate tests

--- peartree/plot.py
+++ peartree/plot.py
@@ -1,18 +1,16 @@
 import os
 
-import matplotlib
 import networkx as nx
 import osmnx as ox
 
-# Check if the display has already been set and, if not...
-cmd = 'python -c "import matplotlib.pyplot as plt;plt.figure()"'
-check = os.system(cmd)
-if check != 0:
-    # Force matplotlib to not use any Xwindows backend
-    matplotlib.use('Agg')
 
+def generate_plot(G: nx.MultiDiGraph, use_agg=False):
+    # Load matplotlib only when plot requested
+    import matplotlib  # noqa
+    if use_agg:
+        # Force matplotlib to not use any Xwindows backend
+        matplotlib.use('Agg')
 
-def generate_plot(G: nx.MultiDiGraph):
     # TODO: Build out custom plotting configurations but,
     #       in the meantime, use OSMnx's plotting configurations
     #       since they work well for the current use case and I
