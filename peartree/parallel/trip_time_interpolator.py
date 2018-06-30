52c040e99c6e3d5c5e63f0094c41e334ddce9f56
Kuan Butts <kuanbutts@gmail.com>
2018-06-29 18:34:58 -0700

    flesh out new class

---
+++ peartree/parallel/trip_time_interpolator.py
@@ -0,0 +1,66 @@
+from multiprocessing.managers import BaseManager
+from typing import Dict, List, Union
+
+import numpy as np
+import pandas as pd
+
+from .utilities import log
+
+
+class NonUniqueSequenceSet(Exception):
+	pass
+
+
+class TripTimeInterpolatorManager(BaseManager):
+    pass
+
+
+class TripTimeInterpolator(object):
+
+    def __init__(
+            self,
+            stop_times_original_df: pd.DataFrame):
+
+        # Initialize common parameters
+        stop_times = stop_times_original_df.copy()
+
+        # Set index on trip id so we can quicly subset the dataframe
+        # during iteration of generate_infilled_times
+        self.stop_times = self.stop_times.set_index('trip_id')
+
+    def generate_infilled_times(self, trip_id: str):
+        # Get all the subset of trips that are related to this route
+        sub_df = self.stop_times.loc[trip_id].copy()
+
+        # TODO: Should we be able to assume that this column is
+        # 		present by the time we arrive here? If so, we should
+        # 		be able to move this check upstream, earlier in tool
+
+	    # Note: Make sure that there is a set of stop sequence
+	    # 		numbers present in each of the trip_id sub-dataframes
+	    if 'stop_sequence' not in sub_df.columns:
+	        sub_df['stop_sequence'] = range(len(sub_df))
+
+	    uniq_sequence_ids = sub_df.stop_sequence.unique()
+	    if not len(uniq_sequence_ids) == len(sub_df):
+	        raise NonUniqueSequenceSet(
+	        	'Expected there to be a unique set of '
+	            'stop ids for each trip_id in stop_times.')
+
+	    # Next, make sure that the subset dataframe is sorted
+	    # stop sequence, incrementing upward
+	    sub_df = sub_df.sort_values(by=['stop_sequence'])
+
+	    # Extract the arrival and departure times as independent arrays
+	    sub_df['arrival_time'] = apply_interpolation(sub_df['arrival_time'])
+	    sub_df['departure_time'] = apply_interpolation(sub_df['departure_time'])
+        return (tst_sub, edge_costs)
+
+
+def apply_interpolation(orig_array):
+    nans, x = nan_helper(orig_array)
+    orig_array[nans] = np.interp(x(nans), x(~nans), orig_array[~nans])
+    return orig_array
+
+
+RouteProcessorManager.register('RouteProcessor', RouteProcessor)
