69761781f77fe44fcb04c8dc9e63d816979247f4
Kuan Butts <kuanbutts@gmail.com>
2018-06-29 19:02:24 -0700

    tabs to spaces

--- peartree/parallel/trip_times_interpolator.py
+++ peartree/parallel/trip_times_interpolator.py
@@ -8,7 +8,7 @@ from peartree.toolkit import nan_helper
 
 
 class NonUniqueSequenceSet(Exception):
-	pass
+    pass
 
 
 class TripTimesInterpolatorManager(BaseManager):
@@ -33,36 +33,36 @@ class TripTimesInterpolator(object):
         sub_df = self.stop_times.loc[trip_id].copy()
 
         # TODO: Should we be able to assume that this column is
-        # 		present by the time we arrive here? If so, we should
-        # 		be able to move this check upstream, earlier in tool
+        #   present by the time we arrive here? If so, we should
+        #   be able to move this check upstream, earlier in tool
 
-	    # Note: Make sure that there is a set of stop sequence
-	    # 		numbers present in each of the trip_id sub-dataframes
-	    if 'stop_sequence' not in sub_df.columns:
-	        sub_df['stop_sequence'] = range(len(sub_df))
+        # Note: Make sure that there is a set of stop sequence
+        #       numbers present in each of the trip_id sub-dataframes
+        if 'stop_sequence' not in sub_df.columns:
+            sub_df['stop_sequence'] = range(len(sub_df))
 
-	    uniq_sequence_ids = sub_df.stop_sequence.unique()
-	    if not len(uniq_sequence_ids) == len(sub_df):
-	        raise NonUniqueSequenceSet(
-	        	'Expected there to be a unique set of '
-	            'stop ids for each trip_id in stop_times.')
+        uniq_sequence_ids = sub_df.stop_sequence.unique()
+        if not len(uniq_sequence_ids) == len(sub_df):
+            raise NonUniqueSequenceSet(
+                'Expected there to be a unique set of '
+                'stop ids for each trip_id in stop_times.')
 
-	    # Next, make sure that the subset dataframe is sorted
-	    # stop sequence, incrementing upward
-	    sub_df = sub_df.sort_values(by=['stop_sequence'])
+        # Next, make sure that the subset dataframe is sorted
+        # stop sequence, incrementing upward
+        sub_df = sub_df.sort_values(by=['stop_sequence'])
 
-	    # Extract the arrival and departure times as independent arrays
-	    sub_df['arrival_time'] = apply_interpolation(sub_df['arrival_time'])
-	    sub_df['departure_time'] = apply_interpolation(sub_df['departure_time'])
+        # Extract the arrival and departure times as independent arrays
+        sub_df['arrival_time'] = apply_interpolation(sub_df['arrival_time'])
+        sub_df['departure_time'] = apply_interpolation(sub_df['departure_time'])
 
-	    # Re-add the trip_id as column at this point
-	    sub_df['trip_id'] = trip_id
+        # Re-add the trip_id as column at this point
+        sub_df['trip_id'] = trip_id
 
-	    # Also, we dump any index set on this subset to avoid issues
-	    # when returned later
-	    sub_df = sub_df.reset_index(drop=True)
+        # Also, we dump any index set on this subset to avoid issues
+        # when returned later
+        sub_df = sub_df.reset_index(drop=True)
 
-	    # Now free to release/return
+        # Now free to release/return
         return sub_df
 
 
