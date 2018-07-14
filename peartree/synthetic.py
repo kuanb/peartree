8e6109624c62d10173d03e738a12dd74a15e7460
Kuan Butts <kuanbutts@gmail.com>
2018-07-13 17:54:17 -0700

    [internal] Preserve units during reprojection (#91)
    * first preserve
    
    * update comments too
    
    * default projections

--- peartree/synthetic.py
+++ peartree/synthetic.py
@@ -12,13 +12,17 @@ from .toolkit import generate_random_name
 def generate_meter_projected_chunks(
         route_shape: LineString,
         custom_stops: List[List[float]]=None,
-        stop_distance_distribution: int=None) -> List[LineString]:
+        stop_distance_distribution: int=None,
+        from_proj='epsg:4326',
+        to_proj='epsg:2163') -> List[LineString]:
 
     # Reproject 4326 lat/lon coordinates to equal area
     project = partial(
         pyproj.transform,
-        pyproj.Proj(init='epsg:4326'),  # source coordinate system
-        pyproj.Proj(init='epsg:2163'))  # destination coordinate system
+        # source coordinate system
+        pyproj.Proj(init=from_proj, preserve_units=True),
+        # destination coordinate system
+        pyproj.Proj(init=to_proj, preserve_units=True))
 
     rs2 = transform(project, route_shape)  # apply projection
 
