cc08db78bf3d6a24ca0f04d2841e843744f1574d
Kuan Butts <kuanbutts@gmail.com>
2018-09-26 13:30:04 -0700

    improve specificity of graph tool conversion tests

--- tests/test_graph_tool.py
+++ tests/test_graph_tool.py
@@ -2,7 +2,7 @@ import json
 import os
 import pytest
 
-from peartree.graph_tool import nx_to_gt
+from peartree.graph_tool import _import_graph_tool, nx_to_gt
 from peartree.paths import load_synthetic_network_as_graph
 from peartree.utilities import config
 
@@ -25,6 +25,23 @@ def test_conversion_to_graph_tool():
     G = load_synthetic_network_as_graph(reference_geojson)
 
     # For now, just run the operation and ensure it completes as intended
-    nx_to_gt(G)
+    gtG = nx_to_gt(G)
 
+    # Make sure that the result is indeed a graph-tool Graph class object
+    gt = _import_graph_tool()
+    assert isinstance(gtG, gt.Graph)
+    
+    # Also make sure that the attributes for all parameters have been preserved
+    assert set(gtG.vp.keys()) == set(('boarding_cost', 'id', 'x', 'y'))
+    assert set(gtG.gp.keys()) == set(('crs', 'name'))
+    assert set(gtG.ep.keys()) == set(('length', 'mode'))
 
+    # Make sure the edge count is the same as the NetworkX graph
+    nx_edges_len = len([x for x in G.edges()])
+    gt_edges_len = len([x for x in gtG.ep['length']])
+    assert nx_edges_len == gt_edges_len
+
+    # And same for the vertices count
+    nx_nodes_len = len([x for x in G.nodes()])
+    gt_nodes_len = len([x for x in gtG.vp['id']])
+    assert nx_nodes_len == gt_nodes_len
