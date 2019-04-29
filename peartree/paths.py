9c87f623ce8ef7f5f3149ba4165c1a2498420f16
Kuan Butts <kuanbutts@gmail.com>
2019-04-29 11:40:56 -0700

    Clarify comment

--- peartree/paths.py
+++ peartree/paths.py
@@ -39,8 +39,7 @@ def _calculate_means_default(arrival_times: np.array) -> float:
     #       would replace this default method
     na = np.array(wait_seconds)
 
-    # Prune all 0-wait times as we want this will overweight to low
-    # wait time estimates
+    # Prune 0-second delays as these excessively reduce wait-time estimates
     na_no_zeroes = na[na > 0]
 
     # Naive implementation: halve the headway to get average wait time
