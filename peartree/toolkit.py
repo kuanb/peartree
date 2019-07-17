7afb0f38b53a1c31a489187902e281402b444040
Kuan Butts <kuanbutts@gmail.com>
2019-07-17 14:51:59 -0700

    add a comment for further reference

--- peartree/toolkit.py
+++ peartree/toolkit.py
@@ -199,9 +199,11 @@ def coalesce(
     edge_summary_method=lambda x: x.max(),
     boarding_cost_summary_method=lambda x: x.mean(),
 ) -> nx.MultiDiGraph:
+    # Note: Feature is experimental. For more details, see
+    #       https://github.com/kuanb/peartree/issues/126
     warnings.warn((
-        'coalesce method is experimental - application risks '
-        'deformation of overall graph structure'))
+        'coalesce method is experimental - method risks '
+        'deformation of relative graph structure'))
 
     # Make sure our resolution satisfies basic requirement
     if resolution < 1:
